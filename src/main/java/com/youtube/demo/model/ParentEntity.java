b90c55af55d0df3f962129190026f2fa0b254424
Natalia Muñoz Zuluaica <natalia.munoz@Quipux.local>
2019-05-05 13:08:10 -0500

    Corrección de error

--- src/main/java/com/youtube/demo/model/ParentEntity.java
+++ src/main/java/com/youtube/demo/model/ParentEntity.java
@@ -17,7 +17,7 @@ public class ParentEntity implements Serializable{
 	 private static final long serialVersionUID = 9022132952590357619L;
 	
 	 @Id
-	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 @Column(name = "id", unique = true, nullable = false)
 	 private Long id;
 
